a8f1aec36eaa02d5dd9b03750f75eafe019eba44
julien jamet <jamet.julien@gmail.com>
2020-06-28 23:13:59 +0200

    feat(core): use average with sensitive params

--- src/index.js
+++ src/index.js
@@ -17,24 +17,46 @@ const GAP = 5;
 
 const DEFAULT_CONF = { gap: GAP, sensibility: MAX_LENGTH_HISTORY };
 
+const getAverageVector = ([first, ...rest]) =>
+    Vector(
+        rest.reduce(
+            (g, { x, y }) => ({
+                x: (g.x + x) / 2,
+                y: (g.y + y) / 2
+            }),
+            first
+        )
+    );
+
 export const MotionSensitive = (params = {}) => {
     const OPTION = { ...DEFAULT_CONF, ...params };
     let history = [];
     const lastIndex = Math.max(MAX_LENGTH_HISTORY, OPTION.sensibility);
     const pushStackLimited = makePushStackLimited(lastIndex + 1);
 
-    const isValidHistory = () => !!(history[0] && history[lastIndex]);
-    const getCurrentVector = () => history[0].sub(history[lastIndex]);
+    const isValidHistoryLenght = () => history.length == lastIndex + 1;
+
+    const getCurrentVector = () => history[0].sub(getAverageVector(history));
+
     const getAngleTargetVector = (pos) => {
         const vector = getCurrentVector();
-        const target = Vector(pos).sub(history[lastIndex]);
+        const target = Vector(pos).sub(getAverageVector(history));
         const angle = getAngle(vector, target);
         return [angle, target];
     };
 
     return {
         isLookedAt: (position = triggerError(errorMessage["isLookedAt"])) => {
-            if (!isValidHistory()) return false;
+            if (isValidHistoryLenght() == false) return false;
+            if (OPTION.sensibility == 4) {
+                let { x, y } = getAverageVector(history);
+                console.log({
+                    sensibility: OPTION.sensibility,
+                    x,
+                    y,
+                    history
+                });
+            }
             const [angle] = getAngleTargetVector(position);
             return angle <= OPTION.gap;
         },
@@ -42,7 +64,7 @@ export const MotionSensitive = (params = {}) => {
             pos1 = triggerError(errorMessage["isLookedIn"]),
             pos2 = triggerError(errorMessage["isLookedIn"])
         ) => {
-            if (!isValidHistory()) return false;
+            if (isValidHistoryLenght() == false) return false;
 
             const [angle1, target1] = getAngleTargetVector(pos1);
             const [angle2, target2] = getAngleTargetVector(pos2);
